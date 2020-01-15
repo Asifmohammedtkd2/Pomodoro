7b7990de09288495740370f2548c69df698cb24b
Asifmohammedtkd2 <asifmohammedn77@gmail.com>
2020-01-15 22:29:33 +0530

    modified get time

--- pomodoro.py
+++ pomodoro.py
@@ -1,57 +1,53 @@
 import time
 from sys import argv
-
-def countdown(time1):
-    list_time=[]
+def time(time1):
     list_mins_secs=[]
-    for times in [time1]:
-        for t in range(times,0,-1):
-            mins,secs = divmod(t, 60)
-            timeformat = '{:02d}:{:02d}'.format(mins, secs)
-            #print(timeformat, end='\n')  
-            list_time.append(timeformat)
-            for i in list_time:
-                sub=i.split(':')
-            list_mins_secs.append(sub)
-            #time.sleep(1)            
+    time1=time1*60
+    for t in range(time1,0,-1):
+        mins,secs = divmod(t, 60)
+        if secs ==0:
+            list_mins_secs.append([mins,secs])
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\n')  
+        #time.sleep(1)
     return list_mins_secs
+print(time(10))
 
-def work(time1):
-    times=countdown(time1)
-    for i in times:
-        mins=int(i[0])
-        secs=int(i[1])
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        #time.sleep(1)            
-        print(timeformat)
-        if mins == 0 and secs == 10:
-            return "Work! 25 minutes"
-        elif mins == 0 and secs == 5:
-            return "10 minutes left"
-        else:
-            return "."
 
-def rest(time1):
-    times=countdown(time1)
-    for i in times:
-        mins=int(i[0])
-        secs=int(i[1])
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        #time.sleep(1)            
-        print(timeformat)
-        if mins == 0 and secs == 5:
-            return "Rest! 5 minutes"
-        else:
-            return "."
-        
-if __name__ == "__main__":
-    time1=10
-    for i in range(time1,0,-1):
-        print(work(i))
-    print("TimeUp")
-    time2=5
-    for i in range(time2,0,-1):
-        print(rest(i))
-    print("TimeUp")
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- test_pomodoro.py
+++ test_pomodoro.py
@@ -1,19 +1,7 @@
-from pomodoro import countdown,work,rest
+from pomodoro import time
 
-def test_count_time():
+def test_get_time():
     time1=10
-    assert countdown(time1) ==  [['00', '10'], ['00', '09'], ['00', '08'], ['00', '07'], ['00', '06'], ['00', '05'], ['00', '04'], ['00', '03'], ['00', '02'], ['00', '01']]
+    assert time(time1) ==  [[10, 0], [9, 0], [8, 0], [7, 0], [6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0]]
+
     
-def test_work_25_minutes():
-    time1=10
-    assert work(time1) == "Work! 25 minutes"
-
-def test_work_10_minutes():
-    time1=5
-    assert work(time1) == "10 minutes left"
-
-def test_rest_time():
-    time2=5
-    assert rest(time2) == "Rest! 5 minutes"
-    
-
