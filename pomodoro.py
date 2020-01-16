5cfdb2cef25dffd0f733a18b62b259cbf77f0c92
Asifmohammedtkd2 <asifmohammedn77@gmail.com>
2020-01-16 12:19:20 +0530

    pomodoro get time funtion

--- pomodoro.py
+++ pomodoro.py
@@ -1,19 +1,26 @@
 import time
 from sys import argv
-def time(time1):
+def pomodoro(time1):
     list_mins_secs=[]
-    time1=time1*60
-    for t in range(time1,0,-1):
-        mins,secs = divmod(t, 60)
-        if secs ==0:
-            list_mins_secs.append([mins,secs])
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print(timeformat, end='\n')  
-        #time.sleep(1)
-    return list_mins_secs
-print(time(10))
+    mins,secs = divmod(time1, 60)
+    timeformat = '{:02d}:{:02d}'.format(mins, secs)
+    print(timeformat, end='\n')
+    #time.sleep(1)
+    return [mins,secs]
 
-
+if __name__== "__main__":
+    for t in range(30*60,0,-1):
+       clock=pomodoro(t)
+       if clock[0] == 30:
+           print("Work! 25 minutes")
+       elif clock[0] == 15 and clock[1] == 0:
+           print("10 minutes left")
+       elif clock[0]==5 and clock[1]==1:
+           print("TimeUp")
+       elif clock[0]==5 and clock[1]==0:
+           print("Rest! 5 minutes")
+       elif clock[0]==0 and clock[1]==1:
+           print("Time up!")
 
 
 
--- test_pomodoro.py
+++ test_pomodoro.py
@@ -1,7 +1,9 @@
-from pomodoro import time
+from pomodoro import pomodoro
 
 def test_get_time():
     time1=10
-    assert time(time1) ==  [[10, 0], [9, 0], [8, 0], [7, 0], [6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0]]
+    assert pomodoro(time1) == [0, 10]
+
+
 
     
