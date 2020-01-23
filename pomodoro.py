2f3811b4925d907b63804d4f8cd0a5999dbfcc8a
Asifmohammedtkd2 <asifmohammedn77@gmail.com>
2020-01-23 14:33:03 +0530

    add argv.Then number of time pomodoro work

--- pomodoro.py
+++ pomodoro.py
@@ -3,9 +3,9 @@ from sys import argv
 def mins_secs(seconds):
         mins=seconds//60
         secs=0
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print(timeformat)
-        time.sleep(1)
+        # timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        # print(timeformat)
+        time.sleep(300)
         return [mins,secs]
             
 def work(seconds):
